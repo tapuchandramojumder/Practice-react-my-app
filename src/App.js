880f4c53f526c155a8aa89d3f77c9bc24f9ee20f
tapuchandramojumder <mojumdertapu@gmail.com>
2020-04-05 11:13:03 +0600

    second commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <p>my first Paragraph</p>
+          <h3>Ready to practice React.js</h3>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
